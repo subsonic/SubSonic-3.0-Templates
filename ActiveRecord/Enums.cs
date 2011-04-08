9f42b00b089ec67ac7064138b571cf0682fb1cce
ben mcintyre <email.ben.mcintyre@gmail.com>
2011-04-09 00:32:16 +0930

    Enum Templates

---
+++ ActiveRecord/Enums.cs
@@ -0,0 +1,32 @@
+
+
+
+ 
+namespace Southwind { 
+
+		// enum derived from database rows: Employees.LastName, Employees.EmployeeID
+		public enum EmployeesEnum {
+			Buchanan = 5,
+			Callahan = 8,
+			Davolio = 1,
+			Dodsworth = 9,
+			Fuller = 2,
+			King = 7,
+			Leverling = 3,
+			Peacock = 4,
+			Suyama = 6
+		}
+
+		// enum derived from database rows: Categories.CategoryName, Categories.CategoryID
+		public enum CategoriesEnum {
+			Beverages = 1,
+			Condiments = 2,
+			Confections = 3,
+			Dairy_Products = 4,
+			Grains_Cereals = 5,
+			Meat_Poultry = 6,
+			Produce = 7,
+			Seafood = 8
+		}
+ 
+}
